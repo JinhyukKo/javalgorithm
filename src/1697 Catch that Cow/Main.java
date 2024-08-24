004884a580c073d7391c15ded5e3398781197f6c
Jinhyuk <kobin1970@gmail.com>
2024-08-24 20:35:38 +0200

    1697 catch that cow

--- src/1697 Catch that Cow/Main.java
+++ src/1697 Catch that Cow/Main.java
@@ -13,47 +13,28 @@ class Node{
 
 public class Main {
     static boolean[] visited;
-    static Graph graph;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
        int  start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
         visited = new boolean[100001];
-        graph = new Graph(start, end);
-        System.out.println(graph.bfs());
-
-
-
-
+        System.out.println(bfs(start,end));
 
     }
-    static class Graph {
-        int start;
-        int end;
 
 
-        public Graph(int start,int end) {
-            this.start=start;
-            this.end=end;
-
-        }
-
-
-         public int bfs(){
+        static public int bfs(int start,int end){
              if (start == end) return 0;
              Queue<Node> queue = new LinkedList<>();
-            queue.offer(new Node(this.start,0));
+            queue.offer(new Node(start,0));
             while (!queue.isEmpty()) {
                 Node node =  queue.poll();
                 int[] nextPositions = {node.x - 1, node.x + 1, node.x * 2};
-
-
                 for (int neighbor : nextPositions) {
                     if (neighbor < 0 || neighbor > 100000|| visited[neighbor]) continue;
-                    if(neighbor == this.end) return node.y+1;
+                    if(neighbor == end) return node.y+1;
                     visited[neighbor] = true;
-
                     queue.offer(new Node(neighbor,node.y+1));
 
                 }
@@ -63,4 +44,3 @@ public class Main {
         }
     }
 
-}
